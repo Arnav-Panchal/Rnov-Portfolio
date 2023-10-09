9bdbe326552f0c4ae6ec88daa2faa786f5efda01
Arnav Panchal <arnavpanchal27@gmail.com>
2023-10-09 23:34:04 +0530

    UPDATE-Menu page

--- components/Header.js
+++ components/Header.js
@@ -52,8 +52,10 @@ export default function Header() {
                 <h1>
                   About Me
                 </h1>
-                <h1>
-                  Resume
+                <h1><a
+                       href="public/Resume.pdf" download="Arnav.pdf">
+                        Resume
+                   </a>
                 </h1>
               </div>
             </div>
@@ -62,16 +64,7 @@ export default function Header() {
             <nav className="py-10 mb-12 flex justify-between dark:text-white">
                 <Image src={favicon} className="h-16 w-16 " href="cursor-pointer"/>
                 <div className="flex items-center">
-                <ul className="flex items-center">
-                    <li>
-                        <a
-                            className="bg-gradient-to-r from-cyan-500 text-to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                                href="public/Resume.pdf"
-                                download="Arnav.pdf"
-                                >Resume
-                        </a>
-                    </li>
-                </ul>
+                
                 <div
               className="cursor-pointer"
               onClick={toggleMenu}
